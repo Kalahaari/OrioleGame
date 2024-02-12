e17c52c862ff56de2f491d701b7d3ef8838cc62c
Pudnangtang <98624581+Pudnangtang@users.noreply.github.com>
2024-02-11 21:53:28 -0500

    Code changes

--- Assets/FoodScript.cs
+++ Assets/FoodScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CaterpillarScript : MonoBehaviour
+public class FoodScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
