cde59217539119e204f5dbaa616d5a0f609285fb
mspaintgod <157856417+mspaintgod@users.noreply.github.com>
2024-02-22 09:26:30 -0500

    season material array assigned

---
+++ Assets/Scripts/SeasonChange.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Whole Level = 10 minutes, Season = 10/3 min
+
+When season changes,
+
+-Goal changes.
+
+1 = Mate
+
+2 = Nest
+
+3 = eat enough to fill expanded energy bar
+
+-2D art on Buildings changes
+
+-Materials on Trees changes
+
+sound effect(?) changes
+*/
+
+public class SeasonChange : MonoBehaviour
+{
+    [SerializeField] int SeasonTimerMax;
+    int SeasonTimer;
+    [SerializeField] Material[] material;
+    private Renderer rend;
+    [SerializeField] int x = 0;
+
+    void Awake()
+    {
+        SeasonTimer = SeasonTimerMax;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+        StartCoroutine(CountDown());
+    }
+
+
+    void Update()
+    {
+        
+    }
+    //set limit for loop also set int
+    //make sure the timer happens twice
+    private IEnumerator CountDown()
+    {
+        while (true)
+        {
+            if(SeasonTimer == 0)
+            {
+                rend.sharedMaterial = material[x + 1];
+                x++;
+                SeasonTimer = SeasonTimerMax;
+            }
+
+            yield return new WaitForSeconds(1);
+            if(SeasonTimer > 0)
+            {
+                SeasonTimer--;
+            }
+            if(x == 2)
+            {
+                break; 
+            }
+        }
+    }
+}
