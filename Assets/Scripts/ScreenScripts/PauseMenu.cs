a5de5af8d5d7a691e5e80f3736918744ecc48dfa
mspaintgod <157856417+mspaintgod@users.noreply.github.com>
2024-02-29 11:09:59 -0500

    finished pause menu

--- Assets/Scripts/ScreenScripts/PauseMenu.cs
+++ Assets/Scripts/ScreenScripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -47,5 +48,12 @@ public class PauseMenu : MonoBehaviour
     public void GotoMainmenu()
     {
         Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitMenu()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
     }
 }
