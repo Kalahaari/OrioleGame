7b431b65670f7e977b02102427adb4ca78da60d9
mspaintgod <157856417+mspaintgod@users.noreply.github.com>
2024-02-29 10:45:29 -0500

    Update PauseMenu.cs

--- Assets/Scripts/ScreenScripts/PauseMenu.cs
+++ Assets/Scripts/ScreenScripts/PauseMenu.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GotoMainmenu()
+    {
+        Time.timeScale = 1f;
     }
 }
