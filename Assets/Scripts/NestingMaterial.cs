c2fa28b22727fc3e4a9e9988c7721dc9af7beee8
Maria <outiedlattie@gmail.com>
2024-04-04 15:12:01 -0400

    New Flapping Sounds

--- Assets/Scripts/NestingMaterial.cs
+++ Assets/Scripts/NestingMaterial.cs
@@ -6,21 +6,27 @@ public class NestingMaterial : MonoBehaviour
 {
     SphereCollider col;
     Rigidbody rb;
+    AudioSource audioSource;
+    [SerializeField] AudioClip[] audioClips;
     // Start is called before the first frame update
     void Start()
     {
         col = GetComponent<SphereCollider>();
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void PickedUp()
     {
+        audioSource.PlayOneShot(audioClips[0]);
         col.enabled = false;
         rb.isKinematic = true;
     }
 
     public void Drop()
     {
+        audioSource.PlayOneShot(audioClips[1]);
         col.enabled = true;
+        rb.isKinematic = false;
     }
 }
